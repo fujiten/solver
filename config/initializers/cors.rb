ad45ea907ec961f6e5f9a8291469a83e2c8ab946
hikaru <hikaru.fujita.0903@gmail>
2019-05-13 22:30:55 +0900

    Set an EnvValuable(Allowed Origin Host) for Front app in Produciton

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost/8080'
+    origins Rails.application.config.x.cors_allowed_origins
 
     resource '*',
       headers: :any,
