7019b6d883cd95fe7e732fd0e33339090caf1498
hikaru <hikaru.fujita.0903@gmail>
2019-05-17 17:38:08 +0900

    Set RAILS_MASTER_KEY ENV on Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   def index
 
     if Rails.env.development?
-      puts "you are on development"
+      puts "you are on development!"
     else
       puts REDIS_URL
-      puts "you are not on development"
+      puts "you are on production!"
     end
 
     @users = User.all 
