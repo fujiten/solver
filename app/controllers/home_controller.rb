76e92766fd7de026e66fa4b68b2f5a538d53b1ee
hikaru <hikaru.fujita.0903@gmail>
2019-05-18 01:31:29 +0900

    Remove question about environment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,5 @@
 class HomeController < ApplicationController
   def index
-
-    if Rails.env.development?
-      puts "you are on development!"
-    else
-      puts REDIS_URL
-      puts "you are on production!"
-    end
-
     @users = User.all 
     render json: @users
   end
