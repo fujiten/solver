4594c858c5eadc767d43090401391014b813c162
hikaru <hikaru.fujita.0903@gmail>
2019-05-17 16:45:11 +0900

    For Checking which developments on Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
-    # puts REDIS_URL
+
+    if Rails.env.development?
+      puts "you are on development"
+    else
+      puts REDIS_URL
+      puts "you are not on development"
+    end
+    
     @users = User.all 
     render json: @users
   end
