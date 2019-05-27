5d231525e290d2dd4f08a5099c04f3418e371ca6
hikaru <hikaru.fujita.0903@gmail>
2019-05-27 18:08:22 +0900

    Add Levelculomn to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_124631) do
+ActiveRecord::Schema.define(version: 2019_05_27_085957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_13_124631) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "level", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
