02c450c9e542916cf21a20bd942bc36880e6265d
hikaru <hikaru.fujita.0903@gmail>
2019-05-29 16:47:38 +0900

    Add User Controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module Api 
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show]
+
+      def show
+        render json: @user
+      end
+
+      private
+
+        def set_user
+          @user = User.find(params[:id])
+        end
+      
+    end
+  end
+end
