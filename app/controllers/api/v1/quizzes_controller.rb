0c39043970804ccbd51ced16d9a55eef98101b34
hikaru <hikaru.fujita.0903@gmail>
2019-05-29 16:47:25 +0900

    Add Show Action

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -1,13 +1,24 @@
 module Api 
   module V1
     class QuizzesController < ApplicationController
+      before_action :set_quiz, only: [:show] 
       
       def index
         @quizzes = Quiz.all
 
         render json: @quizzes
       end
-      
+
+      def show
+        render json: @quiz
+      end
+
+      private
+
+       def set_quiz
+        @quiz = Quiz.find(params[:id])
+       end
+
     end
   end
 end
