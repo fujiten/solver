82e53704bf0f0c9c12687cb04298072ad7636efe
hikaru <hikaru.fujita.0903@gmail>
2019-06-01 19:35:38 +0900

    Fix: Turn off config(cache in Model) for Development Env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.cache_classes = false
+  config.reload_classes_only_on_change = false
 end
 
 REDIS_URL = "redis://redis:6379"
