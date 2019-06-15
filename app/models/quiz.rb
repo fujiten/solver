82979366870667acc42a0a0578f95f77692833cb
hikaru <hikaru.fujita.0903@gmail>
2019-06-15 19:55:23 +0900

    Feat: Add Asociations between Quiz and User

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,8 +2,8 @@ class Quiz < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :queries, dependent: :destroy
 
-  has_many :users, through: :quiz_statuses
   has_many :quiz_statuses, dependent: :destroy
+  has_many :tried_users, through: :quiz_statuses, source: :user
 
   enum published: { drafted: 0, published: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :my_quizzes, class_name: "Quiz", dependent: :destroy
-  has_many :quizzes, through: :quiz_statuses, dependent: :destroy
-  has_many :quiz_statuses 
+  has_many :quiz_statuses, dependent: :destroy
+  has_many :trying_quizzes, through: :quiz_statuses, source: :quiz
+
 end
