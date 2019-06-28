f8ebda893538c2b65d126fd775cdabf7f3f0bbdd
hikaru <hikaru.fujita.0903@gmail>
2019-06-28 15:24:52 +0900

    Debug: Print x_csrf_token For Debug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,14 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :set_user, only: [:show]
-      before_action :authorize_access_request!, only: [:show_mypage]
+      # before_action :authorize_access_request!, only: [:show_mypage]
 
       def show
         render json: @user
       end
 
       def show_mypage
+        p request.x_csrf_token
 
         # draftedとpublishedをそれぞれグループ化してクライアントに返す。
         @my_quizzes = current_user.my_quizzes.group_by{ |quiz| quiz.published }
