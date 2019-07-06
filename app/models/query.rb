7360e24eaa072d247ea6baa5ecfd396f87789972
hikaru <hikaru.fujita.0903@gmail>
2019-07-06 14:22:04 +0900

    Create_Validations_Spec_in_QueryModel

--- app/models/query.rb
+++ app/models/query.rb
@@ -5,4 +5,12 @@ class Query < ApplicationRecord
   has_many :quiz_statuses, through: :query_statuses
 
   enum category: { "不明" => 0, Who: 1, What: 2, When: 3, Where: 4, Why: 5, How: 6}
+
+  validates :body, presence: true,
+                   length: { maximum: 40 }
+  validates :answer, presence: true,
+                     length: { maximum: 40 }
+  validates :revealed_point, numericality: { less_than: 1000 }
+  validates :point, numericality: { less_than: 100 }
+
 end
