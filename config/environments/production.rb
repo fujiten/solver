1e413f08e8cbf1e2db153b7d309af27e30021c1b
hikaru <hikaru.fujita.0903@gmail>
2019-07-12 17:13:33 +0900

    Fix: chenge host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "www.seasolver.club"
+  config.hosts << "api.seasolver.club"
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
