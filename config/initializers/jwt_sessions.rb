e68638a870f67c8916c09b0f3a9cdf82545f2f62
hikaru <hikaru.fujita.0903@gmail>
2019-07-19 17:35:36 +0900

    Chore: Set RedisURL for test environment

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -4,6 +4,6 @@ else
   JWTSessions.encryption_key = 'secret'
 end
 
-if Rails.env.development?
+if !Rails.env.production?
   JWTSessions.token_store = :redis, { redis_url: REDIS_URL || "redis://redis:6379" }
 end
