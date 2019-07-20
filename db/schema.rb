7fcdf0d2cadf00456503f75c1a96da89df0e97ad
hikaru <hikaru.fujita.0903@gmail>
2019-07-20 13:24:40 +0900

    Feat: Add Image Column to Quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_005948) do
+ActiveRecord::Schema.define(version: 2019_07_19_085501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_005948) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "published", default: 0, null: false
+    t.text "image_data"
     t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
